ccbd561ec3e96e820bfc8ba8f49787fd100e83fd
unknown <enelry@code.edu.az>
2024-03-15 20:16:44 +0400

    modified view

--- obj/Debug/net8.0/PustokTemp.AssemblyInfo.cs
+++ obj/Debug/net8.0/PustokTemp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PustokTemp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5357e20a56d6130bdb7ff71d42c41933eff7efc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e979c77653d8c82dd164c1bfb918df66bb65f809")]
 [assembly: System.Reflection.AssemblyProductAttribute("PustokTemp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PustokTemp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
